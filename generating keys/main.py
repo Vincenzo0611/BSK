66e712db38b84851c14e4524c1e8478ea08758ba
Vincenzo Piras <vincenzo.piras@o2.pl>
2025-05-21 09:28:06 +0200

    usb selector

--- generating keys/main.py
+++ generating keys/main.py
@@ -1,11 +1,14 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
-from Crypto.Random import get_random_bytes
-from Crypto.Util.Padding import pad
 import hashlib
 import os
+import tkinter as tk
+
+from tkinter import messagebox
+import psutil
+
+from Crypto.Cipher import AES
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad
 
 
 class KeyGenApp:
@@ -14,17 +17,26 @@ class KeyGenApp:
         master.title("Generator Kluczy RSA")
         master.geometry("400x250")
 
+        self.label = tk.Label(master, text="Wybierz USB")
+
+        self.usbs = self.get_usbs()
+
+        if not self.usbs:
+            self.usbs = ["Brak USB"]
+
+        self.usb_var = tk.StringVar()
+        self.usb_var.set(self.usbs[0])
+
+        self.list_usbs = tk.OptionMenu(master, self.usb_var, *self.usbs)
+        self.list_usbs.pack(pady=10)
+
+        tk.Button(master, text="Odśwież USB", command=self.usb_refresh).pack(pady=10)
+
         self.label = tk.Label(master, text="Wprowadź PIN:")
         self.label.pack(pady=5)
 
         self.pin_entry = tk.Entry(master, show="*", width=30)
         self.pin_entry.pack(pady=5)
-
-        self.label_n = tk.Label(master, text="Wprowadź nazwe pliku do zapisu:")
-        self.label_n.pack(pady=5)
-
-        self.name_entry = tk.Entry(master, width=30)
-        self.name_entry.pack(pady=5)
 
         self.generate_button = tk.Button(master, text="Wygeneruj i Zapisz Klucze", command=self.generate_keys)
         self.generate_button.pack(pady=10)
@@ -34,14 +46,11 @@ class KeyGenApp:
 
     def generate_keys(self):
         pin = self.pin_entry.get()
-        name = self.name_entry.get()
+        usb_path = self.usb_var.get()
         if not pin:
-            messagebox.showerror("Błąd", "PIN nie może być pusty.")
+            tk.messagebox.showerror("Błąd", "PIN nie może być pusty.")
             return
 
-        if not name:
-            messagebox.showerror("Błąd", "Nazwa nie może być pusta.")
-            return
         self.status_label.config(text="⏳ Trwa generowanie kluczy RSA...")
         self.master.update()
         try:
@@ -58,21 +67,38 @@ class KeyGenApp:
             cipher = AES.new(hashed_pin, AES.MODE_CBC, iv)
             encrypted_private_key = cipher.encrypt(pad(private_key, AES.block_size))
 
-            # Wybór folderu do zapisu
-            #folder_selected = filedialog.askdirectory(title="Wybierz folder (np. pendrive)")
-            #if not folder_selected:
-            #    return
 
-            with open(os.path.join("D:\\", name + "_private_key.enc"), "wb") as f:
+            with open(os.path.join(usb_path, "private_key.enc"), "wb") as f:
                 f.write(iv + encrypted_private_key)
 
-            with open(os.path.join("C:\\key", name + "_public_key.pem"), "wb") as f:
+            with open(os.path.join("C:\\key", "public_key.pem"), "wb") as f:
                 f.write(public_key)
 
             self.status_label.config(text="✅ Klucze zapisane poprawnie!")
         except Exception as e:
-            messagebox.showerror("Błąd", f"Wystąpił błąd: {str(e)}")
+            tk.messagebox.showerror("Błąd", f"Wystąpił błąd: {str(e)}")
             self.status_label.config(text="❌ Wystąpił błąd.")
+    def get_usbs(self):
+        result = []
+        partitions = psutil.disk_partitions()
+        for p in partitions:
+            if "removable" in p.opts:
+                result.append(p.device)
+        return result
+    def usb_refresh(self):
+        self.usbs = self.get_usbs()
+
+        if not self.usbs:
+            self.usbs = ["Brak USB"]
+
+        self.usb_var.set(self.usbs[0])
+
+        menu = self.list_usbs["menu"]
+        menu.delete(0, "end")
+        for u in self.usbs:
+            menu.add_command(label=u, command=lambda value=u: self.usb_var.set(value))
+
+
 
 
 if __name__ == "__main__":
